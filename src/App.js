b30a28a2c7288e5552b88303f2711cadd1d55434
Patrycja Miekina <patrycja.miekina@madetech.com>
2023-02-02 11:44:49 +0000

    add map to application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
+import * as tt from '@tomtom-international/web-sdk-maps'
+
 const App = () => {
+  const mapElement = useRef()
+  const [map, setMap] = useState({})
+
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+    })
+
+    setMap(map)
+  }, [])
+
+
   return (
     <div className="App">
+      {/* <h1>Hi!</h1> */}
+      <div ref={mapElement}></div>
     </div>
   )
 }
