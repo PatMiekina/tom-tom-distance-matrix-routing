3af19870e94b6f12cde5fc3d436f6d84e62ef576
Patrycja Miekina <patrycja.miekina@madetech.com>
2023-02-02 11:48:28 +0000

    add styling and center on a location

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import * as tt from '@tomtom-international/web-sdk-maps'
 const App = () => {
   const mapElement = useRef()
   const [map, setMap] = useState({})
+  const latitude = 50.054400
+  const longitude = 19.894044
 
   useEffect(() => {
     let map = tt.map({
       key: process.env.REACT_APP_TOM_TOM_API_KEY,
       container: mapElement.current,
+      center: [longitude, latitude],
+      zoom: 14,
     })
 
     setMap(map)
@@ -19,8 +23,7 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* <h1>Hi!</h1> */}
-      <div ref={mapElement}></div>
+      <div ref={mapElement} className="map"></div>
     </div>
   )
 }
